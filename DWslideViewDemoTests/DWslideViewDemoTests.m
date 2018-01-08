cbc156dfdd2f8803ea443a28a3b1e8cff0b84e93
dangwc <dangwc@gz-mstc.com>
2018-01-08 14:25:13 +0800

    Initial Commit

---
+++ DWslideViewDemoTests/DWslideViewDemoTests.m
@@ -0,0 +1,39 @@
+//
+//  DWslideViewDemoTests.m
+//  DWslideViewDemoTests
+//
+//  Created by dangwc on 2018/1/8.
+//  Copyright © 2018年 dangwc. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface DWslideViewDemoTests : XCTestCase
+
+@end
+
+@implementation DWslideViewDemoTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
